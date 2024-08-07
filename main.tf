6579cb98dec6d2e02b48065b8d92678436f859b2
Nodira Urazbaeva <nodiraura@gmail.com>
2024-08-07 02:28:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 resource "aws_iam_user" "users" {
-for_each = var.users
+for_each = toset(var.users)
 name = each. key
 path = "/"
 }
-
 variable "users" {
     type = list(string)
 }
-
 output "username" {
-    value = [for user in aws_aws_iam_user.users: user.name]
-  
+    value = [for user in aws_aws_iam_user.users: user.name] 
 }
